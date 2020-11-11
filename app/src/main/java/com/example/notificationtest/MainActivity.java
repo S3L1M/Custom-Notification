e62f74c4ca3d9d7c4ae958cce2b36cf51e310d52
mohamed selim <mohamed.selim@fci.helwan.edu.eg>
2020-11-11 20:04:00 +0300

    Creating new project with Empty activity

---
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.notificationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Notification will be shown in 5 sec").show();
+    }
+}
