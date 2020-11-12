16b1db11e20aafcf4ecac5663a202af31f15ba6f
Mohamed Selim <dev.m.selim@gmail.com>
2020-11-12 22:16:18 +0300

    adapt notificationBuilder to new custom layout

--- app/src/main/java/com/example/notificationtest/NotificationUtils.java
+++ app/src/main/java/com/example/notificationtest/NotificationUtils.java
@@ -1,23 +1,24 @@
 package com.example.notificationtest;
 
+import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-
+import android.widget.RemoteViews;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
-public class NotificationUtils {
+public class NotificationUtils extends Activity {
 
     private static final int PENDING_INTENT_ID = 0x468E68;
     private static final int NOTIFICATION_ID = 0xA1138;
     private static final String NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
     private static NotificationManager notificationManager;
     private static NotificationCompat.Builder notificationBuilder;
-
+    private static RemoteViews notificationLayout = new RemoteViews("com.example.notificationtest", R.layout.notification);
     private static PendingIntent contentIntent(Context context) {
         Intent startActivityIntent = new Intent(context, EditActivity.class);
         return PendingIntent.getActivity(context, PENDING_INTENT_ID, startActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -36,6 +37,8 @@ public class NotificationUtils {
         notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                .setCustomContentView(notificationLayout)
                 .setContentIntent(contentIntent(context));
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
@@ -44,10 +47,11 @@ public class NotificationUtils {
         }
     }
 
-    public static void updateNotification(String title, String details) {
-        notificationBuilder
-                .setContentTitle(title)
-                .setContentText(details);
+    public static void updateNotification(String title, String details, int progress) {
+        notificationLayout.setTextViewText(R.id.tv_title, title);
+        notificationLayout.setTextViewText(R.id.tv_details, details);
+        notificationLayout.setProgressBar(R.id.pb_progress, 100, progress, false);
+        notificationBuilder.setCustomContentView(notificationLayout);
         notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
