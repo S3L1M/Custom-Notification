3874138d563f39ffe719ff7d3dff78a5a015f461
Mohamed Selim <dev.m.selim@gmail.com>
2020-11-12 20:17:35 +0300

    implement update functionality

--- app/src/main/java/com/example/notificationtest/EditActivity.java
+++ app/src/main/java/com/example/notificationtest/EditActivity.java
@@ -3,12 +3,23 @@ package com.example.notificationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText mTitle;
+    private EditText mDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        mTitle = (EditText) findViewById(R.id.et_title);
+        mDetails = (EditText) findViewById(R.id.et_details);
+    }
+
+    public void onClickUpdate(View view) {
+        NotificationUtils.updateNotification(mTitle.getText().toString(), mDetails.getText().toString());
     }
 }
